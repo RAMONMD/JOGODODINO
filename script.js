d6b9534da38839365effa8f47a0b3a941b225956
RAMONMD <ramonmendes589@gmail.com>
2025-07-31 15:38:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const dino = document.getElementById("dino");
+const obstaculo = document.getElementById("obstaculo");
+let isjumping = false;
+let speed = 10;
+const accleration = 10;
+const interval = 30;
+const gameAreawidth = document.getElementById("gameArea").offsetWidth;
